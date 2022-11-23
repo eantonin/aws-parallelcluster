75274cda01ec3821c958e0d001f0eccf69055172
Edoardo Antonini <eantonin@amazon.com>
2022-11-23 11:17:17 +0100

    Add pre-prints to the update scripts
    Signed-off-by: Edoardo Antonini <eantonin@amazon.com>

--- tests/integration-tests/tests/update/test_update/test_update_slurm/postupdate.sh
+++ tests/integration-tests/tests/update/test_update/test_update_slurm/postupdate.sh
@@ -10,5 +10,6 @@
 # or in the "LICENSE.txt" file accompanying this file.
 # This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
+echo "starting postupdate script"
 echo "postupdate-$1" > /tmp/postupdate_out.txt
 echo "postupdate run OK"
